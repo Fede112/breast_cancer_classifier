41a45987a2c46d400a43098299fa334de8cfec3d
Fede <fedebarone@gmail.com>
2019-08-06 15:20:34 +0200

    Generation of sample_data done

--- run.sh
+++ run.sh
@@ -29,28 +29,28 @@ python3 src/cropping/crop_mammogram.py \
     --num-processes $NUM_PROCESSES
 # rm -r ./sample_output/cropped_images
 
-# echo 'Stage 2: Extract Centers'
-# python3 src/optimal_centers/get_optimal_centers.py \
-#     --cropped-exam-list-path $CROPPED_EXAM_LIST_PATH \
-#     --data-prefix $CROPPED_IMAGE_PATH \
-#     --output-exam-list-path $EXAM_LIST_PATH \
-#     --num-processes $NUM_PROCESSES
+echo 'Stage 2: Extract Centers'
+python3 src/optimal_centers/get_optimal_centers.py \
+    --cropped-exam-list-path $CROPPED_EXAM_LIST_PATH \
+    --data-prefix $CROPPED_IMAGE_PATH \
+    --output-exam-list-path $EXAM_LIST_PATH \
+    --num-processes $NUM_PROCESSES
 
 # echo 'Stage 3: Generate Heatmaps'
-python3 src/heatmaps/run_producer.py \
-    --model-path $PATCH_MODEL_PATH \
-    --data-path $EXAM_LIST_PATH \
-    --image-path $CROPPED_IMAGE_PATH \
-    --batch-size $HEATMAP_BATCH_SIZE \
-    --output-heatmap-path $HEATMAPS_PATH \
-    --device-type $DEVICE_TYPE \
-    --gpu-number $GPU_NUMBER
+# python3 src/heatmaps/run_producer.py \
+#     --model-path $PATCH_MODEL_PATH \
+#     --data-path $EXAM_LIST_PATH \
+#     --image-path $CROPPED_IMAGE_PATH \
+#     --batch-size $HEATMAP_BATCH_SIZE \
+#     --output-heatmap-path $HEATMAPS_PATH \
+#     --device-type $DEVICE_TYPE \
+#     --gpu-number $GPU_NUMBER
 
 
 # F: --model-path: sample_image_model.p:
 # F: --data-path: output-exam-list-path after extracting centers:
 # F: --output-path: image prediction as csv:
-# echo 'Stage 4a: Run Classifier (Image)'
+echo 'Stage 4a: Run Classifier (Image)'
 python3 src/modeling/run_model.py \
     --model-path $IMAGE_MODEL_PATH \
     --data-path $EXAM_LIST_PATH \
